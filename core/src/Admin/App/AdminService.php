93e7da94844b96f1ef33549f590668a13b286bcf
zbigniewcichanski <zcichanski@gmail.com>
2016-12-23 20:13:56 +0100

    ddd structure

---
+++ core/src/Admin/App/AdminService.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @author: Zbyszek Cichanski
+ * Date: 23.12.2016
+ * Time: 20:12
+ */
+
+namespace Core\Admin\App;
+
+class AdminService
+{
+
+}
---
+++ core/src/Owner/Dm/Service/OwnerService.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @author: Zbyszek Cichanski
+ * Date: 23.12.2016
+ * Time: 20:13
+ */
+
+namespace Core\Owner\Dm\Service;
+
+class OwnerService
+{
+
+}
