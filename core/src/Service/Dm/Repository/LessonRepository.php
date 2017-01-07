07e762e7ab21f0c2c633ffd4773e80935b88e068
zbigniewcichanski <zcichanski@gmail.com>
2017-01-07 16:29:01 +0100

    method of Lesson Repository

---
+++ core/src/Service/Dm/Repository/LessonRepository.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @author: Zbyszek Cichanski
+ * Date: 07.01.2017
+ * Time: 16:24
+ */
+
+namespace Core\Service\Dm\Repository;
+
+class LessonRepository
+{
+    public function getTeacherScheduleForDay($teacherId, $date)
+    {
+
+    }
+
+    public function getTeacherScheduleForAWeek($teacherId, $startDate)
+    {
+
+    }
+
+    public function getFreeTermsForDay($date)
+    {
+
+    }
+
+    public function getFreeTermsTeacherThisWeek($teacherId, $startDate)
+    {
+        
+    }
+}
